23260c5e205899be3e9381922a5fbbf6823eb1ef
Marco <marco.silva631@outlook.com>
2021-08-29 20:59:15 -0300

    Criando a estrutura e o primeiro componente Footer

---
+++ prontuweb/src/components/Footer.js
@@ -0,0 +1,12 @@
+import '../components/Footer.css'
+import React from 'react';
+
+const Footer = (props)=>{
+    return(
+        <div className="container">
+            <p>Todos os direitos reservados Prontu e Ponto &reg;</p>
+        </div>
+    )
+}
+
+export default Footer;
